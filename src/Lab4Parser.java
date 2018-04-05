2c61c4670154da31b5f2a231bd4e009055e8db45
Joshua Edwards <joshuatheman555@gmail.com>
2018-04-04 21:38:33 -0600

    Create Lab4Parser.java

---
+++ src/Lab4Parser.java
@@ -0,0 +1,71 @@
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class Lab4Parser {
+
+	public void readXML(String fileName) {
+		try {
+			File file = new File(fileName);
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(file);
+			doc.getDocumentElement().normalize();
+			NodeList nodeLst = doc.getElementsByTagName("Borrowed_by");
+
+			for (int s = 0; s < nodeLst.getLength(); s++) {
+
+				Node fstNode = nodeLst.item(s);
+
+				if (fstNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element sectionNode = (Element) fstNode;
+
+					NodeList memberIdElementList = sectionNode.getElementsByTagName("MemberID");
+					Element memberIdElmnt = (Element) memberIdElementList.item(0);
+					NodeList memberIdNodeList = memberIdElmnt.getChildNodes();
+					System.out.println("MemberID : " + ((Node) memberIdNodeList.item(0)).getNodeValue().trim());
+
+					NodeList secnoElementList = sectionNode.getElementsByTagName("ISBN");
+					Element secnoElmnt = (Element) secnoElementList.item(0);
+					NodeList secno = secnoElmnt.getChildNodes();
+					System.out.println("ISBN : " + ((Node) secno.item(0)).getNodeValue().trim());
+
+					NodeList codateElementList = sectionNode.getElementsByTagName("Checkout_date");
+					Element codElmnt = (Element) codateElementList.item(0);
+					NodeList cod = codElmnt.getChildNodes();
+					System.out.println("Checkout_date : " + ((Node) cod.item(0)).getNodeValue().trim());
+
+					NodeList cidateElementList = sectionNode.getElementsByTagName("Checkin_date");
+					Element cidElmnt = (Element) cidateElementList.item(0);
+					NodeList cid = cidElmnt.getChildNodes();
+					System.out.println("Checkin_date : " + ((Node) cid.item(0)).getNodeValue().trim());
+
+					System.out.println();
+				}
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String args[]) {
+		try {
+
+			Lab4Parser showXML = new Lab4Parser();
+			showXML.readXML(args[0]);
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} // end catch
+
+	}// end main
+
+}// end class
